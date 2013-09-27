fa8e3c503f449b9a87fad636a00ea45379ac3885
Tux <r02factory@gmail.com>
2013-09-26 23:45:23 -0700

    added zoid-homedir.rb

---
+++ zoid-homedir.rb
@@ -0,0 +1,25 @@
+
+#Prompt For Subdirectory
+puts "#{'-' * 30} \n \n \n"
+print 'Please Specify A Subdirectory Name : '
+
+#Variables containing the user specified subdirectory name, user home dir, and combining both to form the new subdirectory path.
+ProvDir = gets.chomp
+HomeDir = Dir.home
+NewDir  = HomeDir + '/' + ProvDir
+
+#Provide user with the full path of the newly created directory.
+puts "#{'-' * 30} \n \n \n"
+print "Files Will Be Copied Into A The New #{NewDir}\n\n\nShall zoid proceed with the file copy? (Enter yes or no): "
+
+Confirm = gets.chomp
+
+#Creation of the New Directory to be included in conditional statement.....time for sleep
+
+puts "\n \n \n"
+Dir.mkdir(HomeDir + '/' + ProvDir)
+end
+
+
+
+
